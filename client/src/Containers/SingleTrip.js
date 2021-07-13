a7717377239d39d3f0b54fc2c475a2e9954885c4
Garret <gwiats@gmail.com>
2021-07-13 14:34:48 -0400

    working on form to add stops

--- client/src/Containers/SingleTrip.js
+++ client/src/Containers/SingleTrip.js
@@ -3,17 +3,36 @@ import { useParams } from 'react-router-dom'
 
 const SingleTrip = () => {
   const [trip, setTrip] = useState([])
+  const [stopName, setStopName] = useState('')
+  const [description, setDescription] = useState('')
+  const [extraStop, setExtraStop] = useState(false)
   const { id } = useParams()
 
   useEffect(() => {
     fetch(`/trips/${id}`)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => setTrip(data))
   })
+
+  const handleChange = (event) => {
+    if (event.target.id === 'S') {
+      setStopName(event.target.value)
+    } else if (event.target.id === 'D') {
+      setDescription(event.taret.value)
+    }
+  }
 
   return(
     <div>
-      <h3>A trip {id} </h3>
+      <h3>{trip.name}</h3>
+      <form>
+        <h4>Plan a stop</h4>
+        <label>Name: </label>
+        <input id='S' value={stopName} onChange={handleChange}></input>
+        <br/>
+        <label>Description: </label>
+        <input id='D' value={description} onChange={handleChange}></input>
+      </form>
     </div>
   )
 }
