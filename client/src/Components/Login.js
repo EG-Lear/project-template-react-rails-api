fde1a59eb6ee37954e5096868e8ca42f88a4c411
Garret <gwiats@gmail.com>
2021-07-13 08:46:38 -0400

    added planned models to back end

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -17,7 +17,7 @@ const Login = () => {
     fetch('/login', {
       method: "POST",
       headers: {
-        "Content-Type": "text/plain"
+        "Content-Type": "application/json"
       },
       body: JSON.stringify({
         username: userName, 
